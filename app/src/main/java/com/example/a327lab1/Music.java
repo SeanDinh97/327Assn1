b3463e687f90f97ee85d82a836b409d598eceb60
Eric Do <ericdo62497@gmail.com>
2019-09-05 00:27:19 -0700

    Created object classes

---
+++ app/src/main/java/com/example/a327lab1/Music.java
@@ -0,0 +1,14 @@
+//Work in Progress
+package com.example.a327lab1;
+
+public class Music {
+    private Release release;
+    private Artist artist;
+    private Song song;
+
+    public Music(Release release, Artist artist, Song song) {
+        this.release = release;
+        this.artist = artist;
+        this.song = song;
+    }
+}
