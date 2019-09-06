c5e73a2266c5253d668d7621f90e1bb954345889
Airrick_ <ericdo62497@gmail.com>
2019-09-06 00:01:11 -0700

    Moved model classes into separate folder / Added json files

---
+++ app/src/main/java/com/example/a327lab1/models/Release.java
@@ -0,0 +1,11 @@
+package com.example.a327lab1.models;
+
+public class Release {
+    private int id;
+    private String name;
+
+    public Release(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/a327lab1/models/User.java
@@ -0,0 +1,15 @@
+package com.example.a327lab1.models;
+
+import java.util.ArrayList;
+
+public class User {
+    private String username;
+    private String password;
+    private ArrayList<Playlist> listOfPlaylists;
+
+    public User(String username, String password, ArrayList<Playlist> listOfPlaylists) {
+        this.username = username;
+        this.password = password;
+        this.listOfPlaylists = listOfPlaylists;
+    }
+}
